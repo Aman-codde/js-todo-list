36ef69e015d5d79d0f35420f7b623c5880e577ff
aman-codde <amandeepk221@gmail.com>
2024-09-11 22:08:16 -0700

    ref: remove unecessary space

--- index.js
+++ index.js
@@ -25,26 +25,22 @@ submitButton.addEventListener('click', (event)=>{
 
 function addTask(task){
     const li = document.createElement('li');
+
     //span element for task
     let spanTask=document.createElement("span");
-   spanTask.classList.add('task');
-   spanTask.textContent=task;
-   li.appendChild(spanTask);
+    spanTask.classList.add('task');
+    spanTask.textContent=task;
+    li.appendChild(spanTask);
   
-    
-
-   
- 
-
- // add a edit button
+    // add a edit button
    let editBtn=document.createElement("button");
    editBtn.textContent= "Edit";
    li.appendChild(editBtn);
 
    //add a delete button
-    let deleteButton=document.createElement("button");
+   let deleteButton=document.createElement("button");
    deleteButton.textContent= "Delete";
    li.appendChild(deleteButton);
-   ul.appendChild(li);
 
+   ul.appendChild(li);
 };
